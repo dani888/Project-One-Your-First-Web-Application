79af8b2477e3f96bbc88cc407d157a8794fe4c1c
Daniel Werminghausen <daniel.werminghausen@gmail.com>
2021-09-14 17:26:41 -0400

    get card values from all types of cards. make new promises to wait for card values from comp and player

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const $card_image = $('#card_image');
 const $val_play = $('#val_play');
 const $val_comp = $('#val_comp');
-const $reamain_play= $('#reamain_play');
+const $remain_play= $('#remain_play');
 let deck;
-let valTotal_play = 0;
-let valTotal_comp = 0;
+let valTotal_playScore = 0;
+let valTotal_compScore = 0;
 
 
 function getGameStarted (){
+  valTotal_playScore = 0;
+  valTotal_compScore = 0;
   const promise = $.ajax({
       url:'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
   });
@@ -23,10 +25,6 @@ function getGameStarted (){
   );
 }
 
-function nextTurn(){
-    getCardPlayer(deck)
-    getCardComputer(deck)
-}
 // player
 function getCardPlayer(data){
   let deck_id = data.deck_id
@@ -34,24 +32,41 @@ function getCardPlayer(data){
       url:'https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=1'
   });
   // computer card
-  promise1.then(
-    (data) => {
-      console.log(data)
-      data.cards.map(card=>{
-        $('#cards_player').html($('<img>',{src:card.image}))
-        console.log(card.value)
-        $val_play.text(card.value);
+  // promise to return the card value
+  let prom = new Promise((resolve, reject) => {
+    promise1.then(
+      (data) => {
+        // console.log(data)
+        data.cards.map(card=>{
+          $('#cards_player').html($('<img>',{src:card.image}))
+          console.log(card.value)
+          if(card.value == "JACK"){
+            card.value = "11";
+          }
+          if(card.value == "QUEEN"){
+            card.value = "12";
+          }
+          if(card.value == "KING"){
+            card.value = "13";
+          }
+          if(card.value == "ACE"){
+            card.value = "13";
+          }
+          $val_play.text(card.value);
+          // to resolve promise of card.value
+          resolve(parseInt(card.value));
+        })
+      },
+      (error) => {
+      console.log('bad request: ', error);
+      reject();
+      }
 
 
-      })
-    },
-    (error) => {
-    console.log('bad request: ', error);
-    }
-
-
-    
-  );
+      
+    );
+  });
+  return prom;
 }
 // computer
 function getCardComputer(data){
@@ -60,20 +75,59 @@ function getCardComputer(data){
       url:'https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=1'
   });
 
-  promise2.then(
-    (data) => {
-      console.log(data)
-      document.getElementById('reamain_play').innerHTML = (data.remaining / 2);
-      data.cards.map(card=>{
-        $('#cards_computer').html($('<img>',{src:card.image}))
-        $val_comp.text(card.value);  
-        // let val_comp = card.value;
-        //  console.log(val_comp);
-      })
-    },
-    (error) => {
-    console.log('bad request: ', error);
-    }
-  );
+  let prom = new Promise((resolve, reject) => {
+    promise2.then(
+      (data) => {
+        // console.log(data)
+        document.getElementById('remain_play').innerHTML = (data.remaining / 2);
+        data.cards.map(card=>{
+          $('#cards_computer').html($('<img>',{src:card.image}))
+          if(card.value == "JACK"){
+            card.value = "11";
+          }
+          if(card.value == "QUEEN"){
+            card.value = "12";
+          }
+          if(card.value == "KING"){
+            card.value = "13";
+          }
+          if(card.value == "ACE"){
+            card.value = "13";
+          }
+          $val_comp.text(card.value);
+          // to resolve promise of card.value
+          resolve(parseInt(card.value));
+        })
+      },
+      (error) => {
+      console.log('bad request: ', error);
+      reject();
+      }
+    );
+  })
+  return prom;
 }
 
+function scoreBoard(play_score,comp_score){
+  if(play_score > comp_score){
+    console.log('player won!')
+    valTotal_playScore = valTotal_playScore += 1;
+    console.log('Player Score: ' + valTotal_playScore);
+    console.log('Computer Score: ' + valTotal_compScore);
+  }else {
+    console.log('Computer won!')
+    valTotal_compScore = valTotal_compScore += 1;
+    console.log('Player Score: ' + valTotal_playScore);
+    console.log('Computer Score: ' + valTotal_compScore);
+  }
+}
+// function render(){
+
+// }
+async function nextTurn(){
+  // wait for the play card value
+    let play_score = await getCardPlayer(deck)
+  // wait for the comp card value
+    let comp_score = await getCardComputer(deck)
+    scoreBoard(play_score,comp_score)
+}
