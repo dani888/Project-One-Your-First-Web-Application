bcd1a555316368075db65e34dcf13b17f25f523c
Daniel Werminghausen <daniel.werminghausen@gmail.com>
2021-09-15 13:55:31 -0400

    add points system / how they are distributed

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const $remain_play= $('#remain_play');
 let deck;
 let valTotal_playScore = 0;
 let valTotal_compScore = 0;
-
+let prevCard_play = null;
+let prevCard_comp = null;
 
 function getGameStarted (){
   valTotal_playScore = 0;
@@ -39,17 +40,13 @@ function getCardPlayer(data){
         // console.log(data)
         data.cards.map(card=>{
           $('#cards_player').html($('<img>',{src:card.image}))
-          console.log(card.value)
           if(card.value == "JACK"){
             card.value = "11";
           }
           if(card.value == "QUEEN"){
             card.value = "12";
           }
-          if(card.value == "KING"){
-            card.value = "13";
-          }
-          if(card.value == "ACE"){
+          if(card.value == "KING" || card.value == "ACE"){
             card.value = "13";
           }
           $val_play.text(card.value);
@@ -61,13 +58,11 @@ function getCardPlayer(data){
       console.log('bad request: ', error);
       reject();
       }
-
-
-      
     );
   });
   return prom;
 }
+
 // computer
 function getCardComputer(data){
   let deck_id = data.deck_id
@@ -88,10 +83,7 @@ function getCardComputer(data){
           if(card.value == "QUEEN"){
             card.value = "12";
           }
-          if(card.value == "KING"){
-            card.value = "13";
-          }
-          if(card.value == "ACE"){
+          if(card.value == "KING" || card.value == "ACE"){
             card.value = "13";
           }
           $val_comp.text(card.value);
@@ -109,21 +101,44 @@ function getCardComputer(data){
 }
 
 function scoreBoard(play_score,comp_score){
+  if(play_score == comp_score){
+    prevCard_play = play_score;
+    prevCard_comp = comp_score;
+    valTotal_playScore = valTotal_playScore += 1;
+    valTotal_compScore = valTotal_compScore += 1;
+    return 0;
+  }
   if(play_score > comp_score){
     console.log('player won!')
+    if(prevCard_play != null && prevCard_comp != null){
+      valTotal_playScore = valTotal_playScore += 1;
+      valTotal_compScore = valTotal_compScore -= 1;
+      prevCard_play, prevCard_comp = null;
+      render();
+    }
     valTotal_playScore = valTotal_playScore += 1;
     console.log('Player Score: ' + valTotal_playScore);
     console.log('Computer Score: ' + valTotal_compScore);
+    render();
   }else {
     console.log('Computer won!')
+    if(prevCard_play != null && prevCard_comp != null){
+      valTotal_playScore = valTotal_playScore -= 1;
+      valTotal_compScore = valTotal_compScore += 1;
+      prevCard_play, prevCard_comp = null;
+      render();
+    }
     valTotal_compScore = valTotal_compScore += 1;
     console.log('Player Score: ' + valTotal_playScore);
     console.log('Computer Score: ' + valTotal_compScore);
+    render();
   }
 }
-// function render(){
+function render(){
+  document.getElementById("player_total").innerHTML = 'Play Score: ' + valTotal_playScore;
+  document.getElementById("computer_total").innerHTML = 'Comp Score: ' + valTotal_compScore;
+}
 
-// }
 async function nextTurn(){
   // wait for the play card value
     let play_score = await getCardPlayer(deck)
